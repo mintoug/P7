2484a88265df0d19ff3d779477f7c9d754021d6b
Mandhouj A <nouitou@yahoo.fr>
2021-09-16 15:00:25 +0200

    affichage recherche

--- docs/js/dropdown.js
+++ docs/js/dropdown.js
@@ -1,45 +1,55 @@
 import { recipes } from "./recipes.js";
 
 
-let appliances = document.querySelector('.appareils-list');
+let appliances = document.querySelector('.appliance-list');
 let ustensils = document.querySelector('.ustensiles-list');
 let Ingredients = document.querySelector('.ingredients-list');
 
-let chevron = document.querySelectorAll('.fa-chevron-down');
-
-// dislay-on the drop-down on click
-
-
-
+let inputs = document.querySelectorAll('.inputs');
+let inputIngredient = document.querySelector('.button-blue');
+let inputAppliance =  document.querySelector('.button-green');
+let inputUsetensils = document.querySelector('.button-red');
 
 
+inputAppliance.addEventListener('keyup', showAppliances());
 
-
-//console.log(ingredientArray[1])
-function showAppliances() {
-    for (let i = 0; i < recipes.length; i++) {
-
-        appliances.innerHTML += `<li>${recipes[i].appliance}</li>`;
+ function showAppliances() {
+             for (let i = 0; i < recipes.length; i++) {
         
-    }}
-
-function showUstensils() {
-    for (let i = 0; i < recipes.length; i++) {
-    
-        ustensils.innerHTML += `<li>${recipes[i].ustensils}</li>`;
-    
-}}
-
-function showIngredients() {
-    for (let j = 0; j < recipes.length; j++) {
-        let ingredientArray =  recipes[j].ingredients;
-        for (let i = 0; i < ingredientArray.length; i++) {        
-             Ingredients.innerHTML += `<li>${ingredientArray[i].ingredient}</li>`; 
-        }
-    }
+                appliances.innerHTML += `<span>${recipes[i].appliance}</span>`;
 }
 
+    }
+    
+    
+    
+    
+    
+    // //console.log(ingredientArray[1])
+// function showAppliances() {
+//     for (let i = 0; i < recipes.length; i++) {
+
+//         appliances.innerHTML += `<span>${recipes[i].appliance}</span>`;
+        
+//     }}
+
+// function showUstensils() {
+//     for (let i = 0; i < recipes.length; i++) {
+    
+//         ustensils.innerHTML += `<li>${recipes[i].ustensils}</li>`;
+    
+// }}
+
+// function showIngredients() {
+//     for (let j = 0; j < recipes.length; j++) {
+//         let ingredientArray =  recipes[j].ingredients;
+//         for (let i = 0; i < ingredientArray.length; i++) {        
+//              Ingredients.innerHTML += `<li>${ingredientArray[i].ingredient}</li>`; 
+//         }
+//     }
+// }
 
 
 
-//  export {showIngredients, showAppliances, showUstensils}
+
+// export {showIngredients, showAppliances, showUstensils}
