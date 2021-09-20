e609cb210fd4ff9682ab780e48f55f07d9c00138
Mandhouj A <nouitou@yahoo.fr>
2021-09-20 10:31:29 +0200

    avancement premier algorythme

--- docs/js/dropdown.js
+++ docs/js/dropdown.js
@@ -5,36 +5,35 @@ let appliances = document.querySelector('.appliance-list');
 let ustensils = document.querySelector('.ustensiles-list');
 let Ingredients = document.querySelector('.ingredients-list');
 
-
-
-
+ 
+showAppliances(); 
+showUstensils();
+showIngredients();
     
     
-    
-    
-    // //console.log(ingredientArray[1])
-// function showAppliances() {
-//     for (let i = 0; i < recipes.length; i++) {
+    //console.log(ingredientArray[1])
+function showAppliances() {
+    for (let i = 0; i < recipes.length; i++) {
 
-//         appliances.innerHTML += `<span>${recipes[i].appliance}</span>`;
+        appliances.innerHTML += `<li>${recipes[i].appliance}</li>`;
         
-//     }}
+    }}
 
-// function showUstensils() {
-//     for (let i = 0; i < recipes.length; i++) {
+function showUstensils() {
+    for (let i = 0; i < recipes.length; i++) {
     
-//         ustensils.innerHTML += `<li>${recipes[i].ustensils}</li>`;
+        ustensils.innerHTML += `<li>${recipes[i].ustensils}</li>`;
     
-// }}
+}}
 
-// function showIngredients() {
-//     for (let j = 0; j < recipes.length; j++) {
-//         let ingredientArray =  recipes[j].ingredients;
-//         for (let i = 0; i < ingredientArray.length; i++) {        
-//              Ingredients.innerHTML += `<li>${ingredientArray[i].ingredient}</li>`; 
-//         }
-//     }
-// }
+function showIngredients() {
+    for (let j = 0; j < recipes.length; j++) {
+        let ingredientArray =  recipes[j].ingredients;
+        for (let i = 0; i < ingredientArray.length; i++) {        
+             Ingredients.innerHTML += `<li>${ingredientArray[i].ingredient}</li>`; 
+        }
+    }
+}
 
 
 
--- docs/js/search.js
+++ docs/js/search.js
@@ -1,45 +1,45 @@
 import { recipes } from "./recipes.js";
-import {recipesListTemplate} from "./index.js"
-
-let inputs = document.querySelectorAll('.inputs');
-let inputIngredient = document.querySelector('.button--blue').value.toLowerCase;
-let inputAppliance =  document.querySelector('.button--green').value.toLowerCase;
-let inputUsetensils = document.querySelector('.button--red').value.toLowerCase;
-let recipesList = document.getElementById('recipes-list');
 
 
-let filteredRecipes=recipes;
+const chevron = document.querySelector('.chevron');
+const inputIngredient = document.querySelector('.button--blue');
+const inputAppliance =  document.querySelector('.button--green');
+const inputUsetensils = document.querySelector('.button--red');
+let inputResult= document.querySelector('#input-result')
+let recipesList = document.querySelector('#recipes-list');
+let filteredRecipes=[];
+let i=0;
 
-
-function filterTheAppliance(){
-  
-     for (let i=0;i<recipes.length;i++ ){
-    if (recipes[i].appliance.toLocaleLowerCase !== inputAppliance){
-    filteredRecipes = recipes.pop(recipes[i])
-      }
-    else {
-        return filteredRecipes = filteredRecipes}
-    
-    }}
-  console.log(filteredRecipes)
-  
-
-
-
-
-
-function showRecipes(){
-    for(let j=0; j<inputs.length; j++){
-inputs[j].addEventListener('input', (e)=>{
-   if( inputs[j].value.length >2 ){
-       filterTheAppliance();
-    displayFilteredRecipes()}
-    
+/*show the input*/
+inputIngredient.addEventListener('input', ()=>{
+   if(inputIngredient.value.length>2){
+    let inputValue = inputIngredient.value;
+    inputResult.innerHTML =inputValue;
+   return tag=inputValue;
+    }
 });
-}}
+
+inputAppliance.addEventListener('input', ()=>{
+    if(inputAppliance.value.length>2){
+     let inputValue = inputAppliance.value;
+     inputResult.innerHTML =inputValue;
+   }
+ });
+ inputUsetensils.addEventListener('input', ()=>{
+    if(inputUsetensils.value.length>2){
+     let inputValue = inputUsetensils.value;
+     inputResult.innerHTML =inputValue;
+     return inputValue=tag}
+ });
 
 
-function displayFilteredRecipes(){
-    recipesList.innerHTML =filteredRecipes.map((recipe) => recipesListTemplate(recipe)).join("");   
-   
-}
+function filterRecipesAppliance(){
+    
+   do{
+        if(recipes[i].appliance.includes(toString(inputResult))){
+         filteredRecipes=filteredRecipes.push(recipes[i]);
+        } else{i++;} 
+    }
+ while(i<recipes.length)}
+ filterRecipesAppliance();
+ console.log(filteredRecipes)
