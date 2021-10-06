4d26f2083032eac145412af0ce296e7733d3d4ab
Mandhouj A <nouitou@yahoo.fr>
2021-10-06 07:36:47 +0200

    main search

--- docs/js/search_algorithme1.js
+++ docs/js/search_algorithme1.js
@@ -2,36 +2,46 @@ import {recipesListTemplate, recipeIngredientsListTemplate} from "./index.js";
 import { recipes } from "./recipes.js";
 
 /** variables*/
-const inputIngredient = document.querySelector('.button--blue');
-const inputAppliance =  document.querySelector('.button--green');
-const inputUsetensils = document.querySelector('.button--red');
-let inputResult= document.querySelector('#input-result');
-let recipesList = document.getElementById('recipes-list');
-let textInput = document.querySelector('.text-input')
-let filteredUstensil=[];
-let filteredRecipes=[];
-let filteredIngredients=[];
+const inputIngredient   = document.querySelector('.button--blue');
+const inputAppliance    =  document.querySelector('.button--green');
+const inputUsetensils   = document.querySelector('.button--red');
+let inputResult         = document.querySelector('#input-result');
+let mainInput           = document.querySelector('.form-control');
+let recipesList         = document.getElementById('recipes-list');
+let textInput           = document.querySelector('.text-input')
+let filteredUstensil    = [];
+let filteredRecipes     =  [];
+let filteredIngredients = [];
 
 /**listen to the input
 /*show the inputs in the span of the top
 /*call to function displaying recipes*/
 
+mainInput.addEventListener('change', ()=>{
+    inputResult.innerHTML = mainInput.value;
+       textInput.style.visibility = "visible";
+         filterRecipesIngredients(mainInput);
+         recipesList.innerHTML = filteredIngredients.map((recipe) => recipesListTemplate(recipe)).join("");
+         }
+)
+
 /**in ingrdient*/
 inputIngredient.addEventListener('input', ()=>{
    if(inputIngredient.value.length>2){
        inputResult.innerHTML = inputIngredient.value;
-       textInput.style.visibility="visible";
+              textInput.style.visibility = "visible";
          filterRecipesIngredients(inputIngredient);
-         recipesList.innerHTML = filteredIngredients.map((recipe) => recipesListTemplate(recipe)).join("");  
+         recipesList.innerHTML = filteredIngredients.map((recipe) => recipesListTemplate(recipe)).join(""); 
+          
     }
 });
 /*in appliance*/
 inputAppliance.addEventListener('input', ()=>{
     if(inputAppliance.value.length>2){
-        inputResult.innerHTML=inputAppliance.value;
-        textInput.style.visibility="visible";
+        inputResult.innerHTML = inputAppliance.value;
+        textInput.style.visibility = "visible";
         filterRecipesAppliance(inputAppliance);
-            recipesList.innerHTML =filteredRecipes.map((recipe) => recipesListTemplate(recipe)).join("");
+            recipesList.innerHTML = filteredRecipes.map((recipe) => recipesListTemplate(recipe)).join("");
         }
  });
 
@@ -39,12 +49,12 @@ inputAppliance.addEventListener('input', ()=>{
  inputUsetensils.addEventListener('input', ()=>{
     if(inputUsetensils.value.length>2){
         //activate the span
-        inputResult.innerHTML=inputUsetensils.value;
+        inputResult.innerHTML = inputUsetensils.value;
         textInput.style.visibility="visible";
         //function to filter
          filterRecipesUstensils(inputUsetensils);
          //show the recipes filtered
-         recipesList.innerHTML =filteredUstensil.map((recipe) => recipesListTemplate(recipe)).join("");
+         recipesList.innerHTML = filteredUstensil.map((recipe) => recipesListTemplate(recipe)).join("");
      }
  });
 
@@ -52,7 +62,7 @@ inputAppliance.addEventListener('input', ()=>{
 /**filter the appliances */
  function filterRecipesAppliance(){
 
-  for (let i=0; i<recipes.length;i++){
+  for (let i=0 ; i<recipes.length ; i++){
       
    if(recipes[i].appliance.toLowerCase() == inputResult.innerHTML.toLowerCase()){
        //test if recipes exists  yet 
@@ -73,12 +83,15 @@ function filterRecipesUstensils(){
   
   for(let i=0; i<ustensilsTable.length; i++){
          for (let elt in ustensilsTable[i]){
-             if(ustensilsTable[i][elt].toLowerCase() === inputResult.innerHTML.toLowerCase()){
+             if(ustensilsTable[i][elt].toLowerCase() == inputResult.innerHTML.toLowerCase()){
       if (!filteredUstensil.includes(recipes[l])){
       filteredUstensil.push(recipes[l])}
-     }}
+     }
+    
     }
-    }console.log(filteredUstensil)
+    }
+    }
+    console.log(filteredUstensil)
  } 
 
 /**filter ingredients */
